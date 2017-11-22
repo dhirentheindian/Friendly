8ea681898880a533f52632e555787dcc0bfbae25
Benson Poli <benson_polican@dlsu.edu.ph>
2017-11-22 10:45:16 +0800

    Printing of String and Variables work. TODO: String declaration

--- src/antlr/MyListener.java
+++ src/antlr/MyListener.java
@@ -22,18 +22,44 @@ public class MyListener extends FRIENDLYBaseListener {
 
     @Override
     public void enterLocalVariableDeclaration(FRIENDLYParser.LocalVariableDeclarationContext ctx) {
-        Value value = new Value(ctx.typeType().primitiveType().getText());
 
-        if(SymbolTableManager.getInstance().getCurrentScope().getVariable(ctx.variableDeclarators().variableDeclarator(0).variableDeclaratorId().getText())!=null){
-            System.out.println("VAR EXISTS :/");
-            return;
+        //only works so far for primitive types
+
+        if(ctx.typeType().primitiveType() != null) {
+            Value value = new Value(ctx.typeType().primitiveType().getText());
+
+            if (SymbolTableManager.getInstance().getCurrentScope().getVariable(ctx.variableDeclarators().variableDeclarator(0).variableDeclaratorId().getText()) != null) {
+                System.out.println("VAR EXISTS :/");
+                return;
+            }
+            value.setValue(ctx.variableDeclarators().variableDeclarator(0).variableInitializer().getText());
+            System.out.println(SymbolTableManager.getInstance().getCurrentScope().getVariablesSize());
+            SymbolTableManager.getInstance().getCurrentScope().addVariable(ctx.variableDeclarators().variableDeclarator(0).variableDeclaratorId().getText(), value);
+            System.out.println(SymbolTableManager.getInstance().getCurrentScope().getVariablesSize());
         }
-        value.setValue(ctx.variableDeclarators().variableDeclarator(0).variableInitializer().getText());
-        System.out.println( SymbolTableManager.getInstance().getCurrentScope().getVariablesSize());
-        SymbolTableManager.getInstance().getCurrentScope().addVariable(ctx.variableDeclarators().variableDeclarator(0).variableDeclaratorId().getText(), value);
-        System.out.println( SymbolTableManager.getInstance().getCurrentScope().getVariablesSize());
-
         super.enterLocalVariableDeclaration(ctx);
+
+    }
+
+    @Override
+    public void enterStatement(FRIENDLYParser.StatementContext ctx) {
+        if(ctx.getText().contains("print(")){
+            if(ctx.StringLiteral() != null){
+                System.out.println(ctx.StringLiteral().getText());
+            }else if(ctx.Identifier() != null){
+                Value v = SymbolTableManager.getInstance().getCurrentScope().getVariable(ctx.Identifier().getText());
+                System.out.println(v.getValue());
+            }
+
+         }
+        super.enterStatement(ctx);
+    }
+
+    @Override
+    public void enterMethodDeclaration(FRIENDLYParser.MethodDeclarationContext ctx) {
+
+        //ctx.Identifier()
+        super.enterMethodDeclaration(ctx);
 
     }
 }
