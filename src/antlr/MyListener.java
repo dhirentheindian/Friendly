9b2fa0140ad79f3bb6546318446b5ba088752e5e
dhirentheindian <dhirendhanani@gmail.com>
2017-11-23 18:44:21 +0800

    printing multiple works

--- src/antlr/MyListener.java
+++ src/antlr/MyListener.java
@@ -65,14 +65,43 @@ public class MyListener extends FRIENDLYBaseListener {
 
 
         if(statement.substring(0,6).equals("print(")){
-            if(ctx.StringLiteral() != null){
-                String toPrint = ctx.StringLiteral().getText();
-                toPrint = toPrint.substring(1,toPrint.length()-1);
-                System.out.println(toPrint);
-            }else if(ctx.Identifier() != null){
-                Value v = SymbolTableManager.getInstance().getCurrentScope().getVariable(ctx.Identifier().getText());
-                System.out.println(v.getValue());
+//            if(ctx.StringLiteral() != null){
+//                String toPrint = ctx.StringLiteral().getText();
+//                toPrint = toPrint.substring(1,toPrint.length()-1);
+//                System.out.println(toPrint);
+//            }else if(ctx.Identifier() != null){
+//                Value v = SymbolTableManager.getInstance().getCurrentScope().getVariable(ctx.Identifier().getText());
+//                System.out.println(v.getValue());
+//            }
+
+            if(ctx.printContent().printExpression().size()==1){
+                if(ctx.printContent().printExpression().get(0).StringLiteral()!=null) {
+                    String line = ctx.printContent().printExpression().get(0).StringLiteral().getText();
+                    line = line.substring(1,line.length()-1);
+                    System.out.println(line);
+                }
+                else {
+                    Value v = SymbolTableManager.getInstance().getCurrentScope().getVariable(ctx.printContent().printExpression().get(0).Identifier().getText());
+                    System.out.println(v.getValue());
+                }
+
+            }else if (ctx.printContent().printExpression().size() > 1){
+                String mumble = "";
+                for (int i = 0; i < ctx.printContent().printExpression().size();i++){
+                    if(ctx.printContent().printExpression().get(i).StringLiteral()!=null) {
+                        String line = ctx.printContent().printExpression().get(i).StringLiteral().getText();
+                        line = line.substring(1, line.length() - 1);
+                        mumble += line;
+                    }
+                    else {
+                        Value v = SymbolTableManager.getInstance().getCurrentScope().getVariable(ctx.printContent().printExpression().get(i).Identifier().getText());
+                        mumble +=  v.getValue();
+                    }
+                }
+                System.out.println(mumble);
             }
+
+
 
          }else if(statement.substring(0,5).equals("scan(")){
 
@@ -83,6 +112,23 @@ public class MyListener extends FRIENDLYBaseListener {
 
 
         }else if(statement.substring(0,2).equals("if")){
+
+            System.out.println(ctx.parExpression().expression().expression().size());
+
+            String parExp = ctx.parExpression().getText();
+            parExp = parExp.substring(1,parExp.length()-1);
+
+            String[] split = parExp.split("eq");
+            if(SymbolTableManager.getInstance().getCurrentScope().getVariable(split[0]) != null){
+                split[0] = SymbolTableManager.getInstance().getCurrentScope().getVariable(split[0]).getValue();
+            }
+            if(SymbolTableManager.getInstance().getCurrentScope().getVariable(split[1]) != null){
+                split[1] = SymbolTableManager.getInstance().getCurrentScope().getVariable(split[1]).getValue();
+            }
+
+            if(split[0].equals(split[1])){
+                System.out.println("post malone!");
+            }
 
 
         }else if(statement.contains("do")){
