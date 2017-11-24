b431562dc0fdd2526e59d419c52350ba31fb887f
dhirentheindian <dhirendhanani@gmail.com>
2017-11-24 23:50:47 +0800

    push malone. Fixed dec & init probs

--- src/antlr/MyListener.java
+++ src/antlr/MyListener.java
@@ -177,6 +177,12 @@ public class MyListener extends FRIENDLYBaseListener {
                         }
                     }
                 }
+            } else if( statement.contains("=")){
+
+                statement = statement.substring(0,statement.length()-1);
+                String split[] = statement.split("=");
+                SymbolTableManager.getInstance().getCurrentScope().getVariable(split[0]).setValue(split[1]);
+
             }
 
         }
